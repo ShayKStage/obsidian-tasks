edfb811c816af4840e8bad3fddd0ca2e8bebcdb5
Shay Eclipse Kirkegaard Stage <shay@shaykstage.dev>
2025-06-11 12:35:00 +0200

    Add basic flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Obsidian Tasks";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    systems.url = "github:nix-systems/default-linux";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+
+      flake = { };
+
+      systems = import inputs.systems;
+      perSystem =
+        {
+          config,
+          pkgs,
+          lib,
+          system,
+          ...
+        }@args:
+        let
+          formatter = pkgs.nixfmt-tree.override {
+            settings = {
+              tree-root-file = "flake.nix";
+            };
+          };
+        in
+        {
+          inherit formatter;
+
+          devShells.default = pkgs.mkShell {
+            name = "Obsidian Tasks";
+            buildInputs = with pkgs; [
+              formatter
+            ];
+          };
+        };
+    };
+}
